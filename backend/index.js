c400b4bfa6c1f6799c276215768fa6c3db1667ff
AngKuKuehs <bryananghe.dev@gmail.com>
2025-04-01 14:19:04 +0800

    feat(db): init MongoDB connection with environment-based config

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log("Testing");
+import dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+
+dotenv.config();
+dotenv.config({ path: '.env' }); 
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Root");
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
