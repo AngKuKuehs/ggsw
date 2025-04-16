dc1406398aebb587f109f5510d358bd8cd1bda54
AngKuKuehs <bryananghe.dev@gmail.com>
2025-04-17 02:52:40 +0800

    fix: actually fix cors

--- backend/index.js
+++ backend/index.js
@@ -22,23 +22,16 @@ connectDB();
 const app = express();
 const frontendUrl = process.env.CLIENT_URL
 
-app.use(cors());
-
-// app.use(cors({
-//     origin: "http://localhost:5173",  // Vite's default dev server
-//     credentials: true,                // Allow cookies to be sent/received
-//   }));
-
-// app.use(cors({
-//   origin: frontendUrl,
-//   credentials: true,                // Allow cookies to be sent/received
-// }));
+app.use(cors({
+  origin: frontendUrl,
+  credentials: true,                // Allow cookies to be sent/received
+}));
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // api routes
 app.use("/api/users", userRoutes);
